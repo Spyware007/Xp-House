64693fb53bec3ec7fb09c21f5ff4b5fa05d8893b
OM GAWANDE <89961974+Spyware007@users.noreply.github.com>
2021-09-27 00:05:39 +0530

    files added

---
+++ assets/contactus/details_entry.php
@@ -0,0 +1,34 @@
+<?php
+$server_name="localhost";
+$username="root";
+$password="";
+$database_name="database123";
+
+$conn=mysqli_connect($server_name,$username,$password,$database_name);
+//now check the connection
+if(!$conn)
+{
+	die("Connection Failed:" . mysqli_connect_error());
+
+}
+
+if(isset($_POST['save']))
+{	
+	 
+	 $email = $_POST['email'];
+	 $phone = $_POST['phone'];
+
+	 $sql_query = "INSERT INTO entry_details (email,phone)
+	 VALUES ('$email','$phone')";
+
+	 if (mysqli_query($conn, $sql_query)) 
+	 {
+		echo "New Details Entry inserted successfully !";
+	 } 
+	 else
+     {
+		echo "Error: " . $sql . "" . mysqli_error($conn);
+	 }
+	 mysqli_close($conn);
+}
+?>
